f18acdf610aec065678f5ddbcb85005f1aaa69da
chiragsawarn <csawarn@gmail.com>
2024-04-21 14:13:45 +0530

    Q3

---
+++ sliding-window/03_max-avg-sum-of-subarray-of-size-k.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/maximum-average-subarray-i/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        // Strategy : find max sum subarray. Then return average of maxSum
+        int n = nums.size(), i=0;
+        double maxSum = INT_MIN, currSum = 0; // INT_MIN maxSum leke chalo, coz negative elements bhi h array mein
+
+        for(int j=0; j<n; ++j){
+            currSum += nums[j];
+
+            if(j-i+1 == k){
+                maxSum = max(maxSum, currSum);
+                currSum -= nums[i];
+                ++i;
+            }
+        }
+
+        return maxSum/k;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
